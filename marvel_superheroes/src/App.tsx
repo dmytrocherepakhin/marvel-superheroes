034cfaebda80aeffdc7a5fe4903b120022627773
Dmytro Cherepakhin <73669008+dmytrocherepakhin@users.noreply.github.com>
2021-08-11 15:26:05 +0300

    general structure was made

--- marvel_superheroes/src/App.tsx
+++ marvel_superheroes/src/App.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import './App.css';
+import ComicsMainWindow from './components/ComicsMainWindow';
+import HeroMainWindow from './components/HeroMainWindow';
 
 function App(): JSX.Element {
 
   return (
 
     <div className="App">
-
+      <HeroMainWindow />
+      <ComicsMainWindow />
     </div>
   );
 }
