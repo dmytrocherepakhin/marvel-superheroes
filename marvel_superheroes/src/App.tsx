c0f23b7975f66ed7a8b1ad8be8de66db5f93be2c
Dmytro Cherepakhin <73669008+dmytrocherepakhin@users.noreply.github.com>
2021-09-03 12:36:40 +0300

    all project - task 3

--- marvel_superheroes/src/App.tsx
+++ marvel_superheroes/src/App.tsx
@@ -1,13 +1,22 @@
 import React from 'react';
 import './App.css';
+import ComicsPage from './components/ComicsPage/ComicsPage';
+import HomePage from './components/HomePage/HomePage';
+import PageNotFound from './components/PageNotFound/PageNotFound';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
 
 function App(): JSX.Element {
-
   return (
-
-    <div className="App">
-
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/comics/:id" component={ComicsPage} />
+          <Route component={PageNotFound} />
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ marvel_superheroes/src/components/ProgressBarIndeterminate.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ProgressBar from '@atlaskit/progress-bar';
+
+const ProgressBarIndeterminate = (): JSX.Element => {
+    return (<div style={{ backgroundColor: "red", border: "1px solid white" }}>
+        <ProgressBar isIndeterminate />
+    </div>
+    );
+}
+
+export default ProgressBarIndeterminate;
